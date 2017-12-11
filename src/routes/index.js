4f5865f5e7a3f5a1a73167486a13c1b26208ad75
Samuel Prado <samuel.prado@strv.com>
2017-12-11 16:53:03 +0100

    Setup nyc to view test coverage

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ const Router = require('koa-router')
 const controllers = require('../controllers')
 
 const router = new Router()
+
 router.get('/', controllers.genre.index)
 router.post('/genre', controllers.genre.create)
 router.patch('/genre/:id', controllers.genre.update)
