550d242075aac5f8c528e6124c7b301afdb63ecf
Samuel Prado <samuel.prado@strv.com>
2017-12-11 21:24:51 +0100

    Refactor error-handler middleware

--- src/middleware/error-handler.js
+++ src/middleware/error-handler.js
@@ -9,30 +9,46 @@ module.exports = {
       await middleware()
     } catch (error) {
       if (error instanceof errors.ServerError) {
-        ctx.status = error.status || 500
-        ctx.body = {
-          type: error.type,
-          message: error.message,
-        }
-        return
-      }
-
-      error.correlationId = uuid.v1()
-      log.error(error, 'Unhandled error')
-
-      ctx.status = 500
-
-      if (config.env === 'production') {
-        ctx.body = {
-          correlationId: error.correlationId,
-          message: 'Unknown error occurred.',
-        }
+        knownErrorHandler(ctx, error)
       } else {
-        ctx.body = {
-          message: error.message,
-          stack: error.stack,
-        }
+        unknownErrorHandler(ctx, error)
       }
     }
+  },
+}
+
+function knownErrorHandler(ctx, error) {
+  ctx.status = error.status || 500
+  ctx.body = {
+    type: error.type,
+    message: error.message,
+  }
+}
+
+function unknownErrorHandler(ctx, error) {
+  error.correlationId = uuid.v1()
+  log.error(error, 'Unhandled error')
+  ctx.status = 500
+  ctx.body = unknownErrorHandlerCtxBody(error)
+}
+
+function unknownErrorHandlerCtxBody(error) {
+  if (config.env === 'production') {
+    return unknownErrorHandlerCtxBodyProd(error)
+  }
+  return unknownErrorHandlerCtxBodyDev(error)
+}
+
+function unknownErrorHandlerCtxBodyProd(error) {
+  return {
+    correlationId: error.correlationId,
+    message: 'Unknown error occurred.',
+  }
+}
+
+function unknownErrorHandlerCtxBodyDev(error) {
+  return {
+    message: error.message,
+    stack: error.stack,
   }
 }
