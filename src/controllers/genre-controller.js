473ff04eef0d4a37eac49627df9adbdc2966653f
Samuel Prado <samuel.prado@strv.com>
2017-12-12 16:26:29 +0100

    Refactor genre-controller to use validation and resource-finder middleware

--- src/controllers/genre-controller.js
+++ src/controllers/genre-controller.js
@@ -1,65 +1,56 @@
-const { isNull, isUndefined } = require('util')
+const compose = require('koa-compose')
 const _ = require('lodash')
-const joi = require('joi')
 const schema = require('../validation/schema')
 const models = require('../database/models')
-const errors = require('../common/errors')
+const validationMiddleware = require('../middleware/validation')
+const resourceFinderMiddleware = require('../middleware/resource-finder')
 
 module.exports = {
-  async index(ctx) {
-    await models.genre
+  index: async ctx => {
+    const genres = await models.genre
       .query()
-      .then(genres => {
-        ctx.status = 200
-        ctx.body = {
-          genres: _.map(genres, genre => {
-            return genre.name
-          }),
-        }
-      })
-  },
-  async create(ctx) {
-    const result = joi.validate(ctx.request.body, schema.genre.schema)
-    if (result.error !== null) {
-      throw new errors.ValidationError()
-    }
-
-    const newGenre = await models.genre
-      .query()
-      .insert(ctx.request.body.genre)
-      .returning('*')
-
-    ctx.status = 201
-    ctx.body = {
-      genre: {
-        name: newGenre.name,
-      },
-    }
-  },
-  async update(ctx) {
-    const genre = await models.genre
-      .query()
-      .findById(ctx.params.id)
-
-    if (isUndefined(genre) || isNull(genre)) {
-      throw new errors.NotFoundError()
-    }
-
-    const result = joi.validate(ctx.request.body, schema.genre.schema)
-    if (result.error !== null) {
-      throw new errors.ValidationError()
-    }
-
-    // updating value
-    const updatedGenre = await models.genre
-      .query()
-      .patchAndFetchById(ctx.params.id, ctx.request.body.genre)
 
     ctx.status = 200
     ctx.body = {
-      genre: {
-        name: updatedGenre.name,
-      },
+      genres: _.map(genres, genre => genre.name),
     }
   },
+
+  create: compose([
+    validationMiddleware.validateBody(schema.genre.schema),
+
+    async ctx => {
+      const newGenre = await models.genre
+        .query()
+        .insert(ctx.request.validatedBody.genre)
+        .returning('*')
+
+      ctx.status = 201
+      ctx.body = {
+        genre: {
+          name: newGenre.name,
+        },
+      }
+    },
+  ]),
+
+  update: compose([
+    resourceFinderMiddleware.ensureResourceFound(models.genre),
+    validationMiddleware.validateBody(schema.genre.schema),
+
+    async ctx => {
+      const genre = ctx.request.foundResource
+      // updating value
+      const updatedGenre = await models.genre
+        .query()
+        .patchAndFetchById(genre.id, ctx.request.validatedBody.genre)
+
+      ctx.status = 200
+      ctx.body = {
+        genre: {
+          name: updatedGenre.name,
+        },
+      }
+    },
+  ]),
 }
