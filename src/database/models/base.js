4ab5bbd5c41e17c9f5641522da3936b1b8d74404
Samuel Prado <samuel.prado@strv.com>
2017-12-20 17:15:57 +0100

    Fix problem with timestamp fields on models

--- src/database/models/base.js
+++ src/database/models/base.js
@@ -1,3 +1,18 @@
+const has = require('lodash/has')
 const db = require('./../../database')
 
-module.exports = db.Model
+class Base extends db.Model {
+  $beforeInsert() {
+    if (has(this, 'created_at')) {
+      this.created_at = new Date().toISOString()
+    }
+  }
+
+  $beforeUpdate() {
+    if (has(this, 'updated_at')) {
+      this.updated_at = new Date().toISOString()
+    }
+  }
+}
+
+module.exports = Base
